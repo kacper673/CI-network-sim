72b02dd194b5f314d1fb4d5bf587197cdd96a99c
Nikodem5 <lskywalker568@gmail.com>
2025-10-04 14:03:01 +0200

    initial commit

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
 class Layer:
     def __init__(self, name, resource_type):
         self.name = name
-        self.resource_type = resource_type
-        self.nodes = set()
+        self.resource_type = resource_type        
         self.edges = []
 
 class Edge:
@@ -11,7 +10,7 @@ class Edge:
         self.to_node = to_node
         self.attributes = attributes # ex. dict: {"capacity": 100, "status": "active"}
 
-class Bulding:
+class Building:
     def __init__(self, id, requires=None, produces=None, priority=1):
         self.id = id
 
@@ -20,4 +19,44 @@ class Bulding:
         self.requires = requires
         self.produces = produces
 
-        self.status = "active" # active offline 
+        self.status = "active" # active offline destroyed
+
+        self.input_supply = {}
+
+class Hospital(Building):
+    def __init__(self, basic_resources, consumption_per_tick):
+        super().__init__()
+        self.basic_resources = basic_resources
+        self.consumption_per_tick = consumption_per_tick        
+
+    def tick(self):
+        if self.basic_resources >= self.consumption_per_tick:
+            self.basic_resources -= self.consumption_per_tick
+            return True
+        else:            
+            self.basic_resources = 0
+            return False        
+
+    def receive_supplies(self, amount):        
+        self.basic_resources += amount            
+
+class Magazine(Building):
+    def __init__(self, basic_resources, supply_per_tick):
+        super().__init__()
+        self.basic_resources = basic_resources
+        self.supply_per_tick = supply_per_tick
+
+    def tick(self):        
+        if self.basic_resources >= self.supply_per_tick:
+            self.basic_resources -= self.supply_per_tick
+            return self.supply_per_tick
+        else:            
+            return 0
+            
+class Road(Layer):
+    def __init__(self, state, capacity, status):
+        super().__init__()
+        self.resource_type = ["personnel", "basic_resources"]
+        self.state = state
+        self.capacity = capacity
+        self.status = status            
